4299e73bff02f49882471a05f8815f88073b8ff3
Ulysse Gérard <thevoodoos@gmail.com>
2020-05-02 23:15:33 +0200

    SImplify bot handler

--- bin/handlers.ml
+++ bin/handlers.ml
@@ -2,36 +2,25 @@ open Lwt.Infix
 open Model.Cache
 include Gateway.Handlers.Default
 
+module List = ListLabels
+
 let on_message ~cache message =
   let open Model.Message in
   Logs.debug (fun m ->
       m "I am a handler doing SOMETHING with the message %s" message.content);
 
-  let find c txt =
-    let upc = char_of_int (int_of_char c - 32) in
-    String.index_opt txt c
-    |> Option.map (fun i ->
-           let byts = Bytes.of_string txt in
-           Bytes.set byts i upc;
-           Bytes.to_string byts)
-  in
-
   if not (Int64.equal message.author.id cache.user.id) then
     let txt = String.lowercase_ascii message.content in
 
-    let bind f o = Option.bind o f in
+  let txt = Bytes.of_string txt in
+  let chars = ['p';'o';'u';'e';'t'] in
+  if (List.for_all ~f:(fun c -> Bytes.contains txt c) chars) then
+    (List.iter chars
+      ~f:(fun c -> Bytes.(set txt (index txt c) (Char.uppercase_ascii c)));
 
-    let res =
-      find 'p' txt
-      |> bind (find 'o')
-      |> bind (find 'u')
-      |> bind (find 'e')
-      |> bind (find 't')
-      |> Option.map (fun s ->
-             let payload = { Model.Actions.Create_message.content = s } in
-             Model.Actions.Create_message.run ~payload message.channel_id
-             >>= fun _ -> Lwt.return cache)
-    in
+    let payload = { Model.Actions.Create_message.content = Bytes.to_string txt } in
+    Model.Actions.Create_message.run ~payload message.channel_id
+  >>= fun _ -> Lwt.return cache)
+   else Lwt.return cache
 
-    match res with None -> Lwt.return cache | Some lwt -> lwt
   else Lwt.return cache
